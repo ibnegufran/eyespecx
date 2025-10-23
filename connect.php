61ba03c2845e6777300f503afd129ff54b12bfd9
Ansari Abdurrahman gufran Ahmed <110079179+ibnegufran@users.noreply.github.com>
2025-10-23 17:44:15 +0530

    server

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$server='localhost';
-$username='root';
-$password='';
+$server='db.fr-pari1.bengt.wasmernet.com';
+$username='a1a52a1d782480004f9aa55c06bc';
+$password='068fa1a5-2a1d-79d0-8000-c17d91b8a426';
 $database='eyespecx';
 
 $con=mysqli_connect($server,$username,$password,$database);
